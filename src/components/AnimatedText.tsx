f0b66cce5add268cc83a47a0558ca005292d19e1
Shaiiko19 <shaiiko19@gmail.com>
2025-08-29 09:45:21 +0200

    fix: fixed motion warning

--- src/components/AnimatedText.tsx
+++ src/components/AnimatedText.tsx
@@ -30,18 +30,18 @@ function AnimatedText({ words, interval = 2000 }: AnimatedTextProps) {
         <motion.span
           key={index}
           className="absolute font-semibold text-blue-400"
-          initial={{ opacity: 0, y: "-100" }}
+          initial={{ opacity: 0, y: "0" }}
           transition={{ type: "spring", stiffness: 50 }}
           animate={
             currentIndex === index
               ? {
-                  y: 0,
-                  opacity: 1,
-                }
+                y: 0,
+                opacity: 1,
+              }
               : {
-                  y: currentIndex > index ? -150 : 150,
-                  opacity: 0,
-                }
+                y: currentIndex > index ? -150 : 150,
+                opacity: 0,
+              }
           }
           data-oid="1rle.-s"
         >
